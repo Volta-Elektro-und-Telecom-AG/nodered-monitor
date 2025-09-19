531628eacee1eb7559ab404f4a8e33fcb66c365e
patrikaeberli <p.aeberli@volta-winterthur.ch>
2025-09-19 15:32:42 +0200

    Update nodered-monitor.sh

--- nodered-monitor.sh
+++ nodered-monitor.sh
@@ -81,6 +81,9 @@ run_cmd "curl -sL \"https://raw.githubusercontent.com/Volta-Elektro-und-Telecom-
 run_cmd "curl -sL \"https://raw.githubusercontent.com/Volta-Elektro-und-Telecom-AG/nodered-monitor/refs/heads/main/volta_logo.webp\" -o /var/www/html/monitoring/pings/volta_logo.webp" "volta_logo.webp heruntergeladen"
 run_cmd "curl -sL \"https://raw.githubusercontent.com/Volta-Elektro-und-Telecom-AG/nodered-monitor/refs/heads/main/monitoring/index.php\" -o /var/www/html/monitoring/index.php" "monitoring/index.php heruntergeladen"
 
+run_cmd "sudo rm /var/www/html/index.php"
+run_cmd "curl -sL \"https://raw.githubusercontent.com/Volta-Elektro-und-Telecom-AG/nodered-monitor/refs/heads/main/index.php\" -o /var/www/html/index.php" "index.php heruntergeladen"
+
 run_cmd "sudo chmod 777 /dev/vcio" "Rechte für /dev/vcio gesetzt"
 
 headline "Installation abgeschlossen 🎉"
